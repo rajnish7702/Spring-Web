b54eec8f9370b6f3c3bcc0626a29da2ba78ba353
Rajnish Pal <rajnish.pal7702@gmail.com>
2022-11-21 22:29:08 +0530

    initial project

---
+++ src/main/java/com/rajnish/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.rajnish.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public ModelAndView getWelcomeMessage() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg", "Welcome to Web MVC");
+		mav.setViewName("index");
+		return mav;
+	}
+}
